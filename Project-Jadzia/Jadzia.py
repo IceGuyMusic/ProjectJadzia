80ba7c0341e5c9aab1742115e0eb9c24f4b007c5
Dominik Jaksch <dominik.jakschefg@gmail.com>
2022-11-03 18:13:18 +0100

    Test Celery

--- Project-Jadzia/Jadzia.py
+++ Project-Jadzia/Jadzia.py
@@ -4,6 +4,8 @@ from flask import Flask, redirect, url_for, render_template, send_file, request,
 from flask_sqlalchemy import SQLAlchemy
 from datetime import timedelta
 from main.config import Config
+from tasks.celery import make_celery
+from celery import Celery
 
 db = SQLAlchemy()
 
@@ -11,6 +13,10 @@ def create_app(config_class=Config):
     app = Flask(__name__)
     app.secret_key = "YC!NWN"
     app.config.from_object(Config)
+    app.config.update(CELERY_CONFIG={
+        'broker_url': 'redis://localhost:6379',
+        'result_backend': 'redis://localhost:6379',
+    })
     app.permanent_session_lifetime = timedelta(days = 3)
     from main.main import main
     app.register_blueprint(main)
@@ -21,6 +27,23 @@ def create_app(config_class=Config):
 
 app = create_app()
 
+app.config.update(CELERY_CONFIG={
+    'broker_url': 'redis://localhost:6379',
+    'result_backend': 'redis://localhost:6379',
+})
+celery = make_celery(app)
+
+
+@app.route("/celery/")
+def addd_API():
+    add_together.delay(1,12)
+    return "Send a Celery"
+
+
+@celery.task(name='Jadzia.add_together')
+def add_together(a,b):
+    results = a + b
+    return results
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ Project-Jadzia/tasks/celery.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+from celery import Celery
+
+def make_celery(app):
+    celery = Celery(app.import_name)
+    celery.conf.update(app.config["CELERY_CONFIG"])
+
+    class ContextTask(celery.Task):
+        def __call__(self, *args, **kwargs):
+            with app.app_context():
+                return self.run(*args, **kwargs)
+
+    celery.Task = ContextTask
+    return celery
