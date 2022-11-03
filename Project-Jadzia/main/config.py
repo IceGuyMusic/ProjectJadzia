3987d66ed306005a543fb6f48f229ad2b81df02d
Dominik Jaksch <dominik.jakschefg@gmail.com>
2022-11-03 11:38:58 +0100

    Imported File Uploader

--- Project-Jadzia/main/config.py
+++ Project-Jadzia/main/config.py
@@ -1,6 +1,10 @@
 #!/bin python
 
+wrk_dir = "/home/labbikatz/ProjectJadzia/ProjectJadzia/Project-Jadzia" 
+
 class Config:
     SQLALCHEMY_DATABASE_URI = 'sqlite:///db.sqlite3'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    UPLOAD_FOLDER = f"{wrk_dir}/uploads/wiff"
+    ALLOWED_EXTENSIONS = ['wiff', 'scan', 'txt'] 
 
--- Project-Jadzia/main/main.py
+++ Project-Jadzia/main/main.py
@@ -1,5 +1,8 @@
 #!/bin python
 from flask import Flask, Blueprint, current_app, redirect, url_for, render_template, session, flash, request
+from werkzeug.utils import secure_filename
+import os
+
 
 main = Blueprint('main', __name__)
 
@@ -8,3 +11,35 @@ def home():
     return render_template('base.html')
 
 
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in current_app.config['ALLOWED_EXTENSIONS']
+
+@main.route("/upload/", methods=['POST', 'GET'])
+def upload():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(current_app.config['UPLOAD_FOLDER'], filename))
+            return f"Succesfull upload for file {filename}"
+    return '''
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''
+
+@main.route('/<name>')
+def error(name):
+    return f"{name} was not found"
