6cd2ea353888421c8c1408ea6d64623e8aafbff7
Dominik Jaksch <dominik.jakschefg@gmail.com>
2023-01-23 15:17:45 +0100

    Modified Path for Windows

--- Project-Jadzia/main/config.py
+++ Project-Jadzia/main/config.py
@@ -5,7 +5,7 @@ from pathlib import Path
 bash_msconvert = "sudo docker run -it --rm -e WINEDEBUG=-all -v /your/data:/data chambm/pwiz-skyline-i-agree-to-the-vendor-liscenses wine msconvert "
 windows_msconvert = Path(r"C:\Users\Biotechnologie\KNIME\knime_3.3.2\plugins\de.openms.win32.x86_64_2.1.0.201704211842\payload\bin\pwiz-bin\msconvert.exe")
 wrk_dir = "/home/labbikatz/ProjectJadzia/ProjectJadzia/Project-Jadzia" 
-windows_wrk_dir = Path(r"C:\Users\Biotechnologie\Documents\Bioinformatik\src\ProjectJadzia\Project-Jadzia")
+windows_wrk_dir = r"C:\Users\Biotechnologie\Documents\Bioinformatik\src\ProjectJadzia\Project-Jadzia"
 
 class Config:
     SQLALCHEMY_DATABASE_URI = 'sqlite:///db.sqlite3'
@@ -13,5 +13,5 @@ class Config:
     UPLOAD_FOLDER = f"{wrk_dir}/uploads/wiff"
     MZML_FOLDER = f"{wrk_dir}/uploads/mzml"
     ALLOWED_EXTENSIONS = ['wiff', 'scan', 'txt'] 
-    WIN_MZML_FOLDER = windows_wrk_dir / r"\uploads\mzml"
-    WIN_UPLOAD_FOLDER = windows_wrk_dir / r"\uploads\wiff"
+    WIN_MZML_FOLDER = windows_wrk_dir + r"\uploads\mzml"
+    WIN_UPLOAD_FOLDER = windows_wrk_dir + r"\uploads\wiff"
